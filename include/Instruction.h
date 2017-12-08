a21b8a75d6d74a0b88393e9a6ebda66d622c9f2f
Joshua Weinstein <jweinstein@splunk.com>
2017-12-08 14:11:54 -0800

    lib work, instructions

---
+++ include/Instruction.h
@@ -0,0 +1,12 @@
+#ifndef LOBSTER_INSTRUCTION_H
+#define LOBSTER_INSTRUCTION_H
+
+typedef enum
+{
+	Instruction_Int,
+	Instruction_Add,
+	Instruction_ExpStart,
+	Instruction_ExpEnd
+} Instruction;
+
+#endif
---
+++ include/Translate.h
@@ -0,0 +1,10 @@
+#ifndef LOBSTER_TRANSLATE_H
+#define LOBSTER_TRANSLATE_H
+// covers source code -> bytes
+
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif
